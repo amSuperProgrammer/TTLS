c6d64f7738d6fff9d0cde2083b45ed63705bc1a7
amSuperProgrammer <amSuperProgrammer@gmail.com>
2022-12-12 11:21:00 +0600

    1231231231232323132131

--- Assets/scripts/redTraffic.cs
+++ Assets/scripts/redTraffic.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class redTraffic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Material redOBJ;
+    [SerializeField] Material yellowOBJ;
+    [SerializeField] Material greenOBJ;
+    [SerializeField] Material redOBJ1;
+    [SerializeField] Material yellowOBJ1;
+    [SerializeField] Material greenOBJ1;
 
-    // Update is called once per frame
+    [SerializeField] GameObject redBlock;
+
+    Collider redCollid;
+
     void Update()
     {
-        
+        redCollid = redBlock.GetComponent<BoxCollider>();
+
+        if (Input.GetKey(KeyCode.E))
+            redChange(redOBJ, yellowOBJ, greenOBJ, redOBJ1, yellowOBJ1, greenOBJ1, redCollid);
+        if(Input.GetKey(KeyCode.Q))
+            greenChange(redOBJ, yellowOBJ, greenOBJ, redOBJ1, yellowOBJ1, greenOBJ1, redCollid);
+    }
+
+    static void redChange(Material red, Material yellow, Material green, Material red1, Material yellow1, Material green1, Collider redBlock)
+    {
+        red.color = new Color(255f, 0f, 0f, 0f);
+        yellow.color = new Color(0f, 0f, 0f, 0f);
+        green.color = new Color(0f, 0f, 0f, 0f);
+        red1.color = new Color(0f, 0f, 0f, 0f);
+        yellow1.color = new Color(0f, 0f, 0f, 0f);
+        green1.color = new Color(0f, 255f, 0f, 0f);
+        redBlock.enabled = true;
+    }
+
+    static void greenChange(Material red, Material yellow, Material green, Material red1, Material yellow1, Material green1, Collider redBlock)
+    {
+        red.color = new Color(0f, 0f, 0f, 0f);
+        yellow.color = new Color(0f, 0f, 0f, 0f);
+        green.color = new Color(0f, 255f, 0f, 0f);
+        red1.color = new Color(255f, 0f, 0f, 0f);
+        yellow1.color = new Color(0f, 0f, 0f, 0f);
+        green1.color = new Color(0f, 0f, 0f, 0f);
+        redBlock.enabled = false;
     }
 }
